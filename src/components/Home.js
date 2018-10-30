1253377717371b40ee8238c6c949a70db5486b09
Jason Park <binarydiver@gmail.com>
2018-10-30 18:12:47 +0900

    Add pricing section and wireguard description.

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,14 +4,17 @@ import { withStyles } from '@material-ui/core/styles';
 import {
   AppBar,
   Button,
+  Divider,
   Grid,
   IconButton,
+  Paper,
   Toolbar,
   Typography,
 } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
 import CloudDownloadIcon from '@material-ui/icons/CloudDownload';
 import img0502 from '../assets/anakwa_tothesememories.jpg';
+import logoWireguard from '../assets/wireguard.svg';
 
 const styles = (theme) => ({
   root: {
@@ -38,6 +41,13 @@ const styles = (theme) => ({
     fontSize: '1.1em',
     fontWeight: 'bold',
   },
+  paper: {
+    padding: theme.spacing.unit * 2,
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+    width: 200,
+  },
+  description: theme.typography.body2
 });
 
 class Home extends React.Component {
@@ -48,7 +58,7 @@ class Home extends React.Component {
     return (
       <div className={classes.root}>
         <div style={{zIndex: -1, position: 'absolute', top: 0, width:'100%', height: 500, backgroundImage: `url(${img0502})`, backgroundRepeat:'no-repeat', backgroundSize: 'cover', backgroundPosition: 'center'}}></div>
-        <AppBar position="sticky" className={classes.appBar} color="default">
+        <AppBar className={classes.appBar} color="default">
           <Toolbar>
             <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
               <MenuIcon />
@@ -56,19 +66,93 @@ class Home extends React.Component {
             <Typography variant="h6" color="inherit" className={classes.grow}>
               Uncharted VPN
             </Typography>
-            <Button color="inherit">Login</Button>
+            <Button color="inherit">Login / SignUp</Button>
           </Toolbar>
         </AppBar>
         <div className={classes.root}>
-          <Grid container spacing={24}>
-            <Grid item xs={12} alignItems="center" justify="center">
-              <div item style={{color: 'white', fontSize: 60, fontWeight: 'bold', padding: '200px 0 20px 0'}}>
+          <Grid container alignItems="center" justify="center" style={{ height: 500 }}>
+            <Grid item xs={12}>
+              <div item style={{color: 'white', fontSize: '4em', fontWeight: 'bold', padding: '160px 0 20px 0'}}>
                 Reliable VPN Service
               </div>
               <Button variant="extendedFab" className={classes.button}>
                 <CloudDownloadIcon className={classes.extendedIcon} />
                 Download Now
               </Button>
+            </Grid>
+          </Grid>
+          <Grid container spacing={24} xs={12} justify="center" style={{marginTop: -50}}>
+            <Grid item>
+              <Paper className={classes.paper}>
+                <Grid container justify="center" spacing={16} direction="column">
+                  <Grid item>
+                    <b>1 Month</b>
+                  </Grid>
+                  <Divider />
+                  <Grid item>
+                    $5.0 / mo
+                  </Grid>
+                  <Grid item>
+                    $5.0
+                  </Grid>
+                  <Grid item>
+                    5 Devices
+                  </Grid>
+                  <Button variant="outlined" fullWidth={true}>
+                    Start Now
+                  </Button>
+                </Grid>
+              </Paper>
+            </Grid>
+            <Grid item>
+              <Paper className={classes.paper}>
+                <Grid container justify="center" spacing={16} direction="column">
+                  <Grid item>
+                    <b>3 Month</b>
+                  </Grid>
+                  <Divider />
+                  <Grid item>
+                    $4.0 / mo
+                  </Grid>
+                  <Grid item>
+                    $12.0
+                  </Grid>
+                  <Grid item>
+                    5 Devices
+                  </Grid>
+                  <Button variant="outlined" fullWidth={true}>
+                    Start Now
+                  </Button>
+                </Grid>
+              </Paper>
+            </Grid>
+            <Grid item>
+              <Paper className={classes.paper}>
+                <Grid container justify="center" spacing={16} direction="column">
+                  <Grid item>
+                    <b>1 Year</b>
+                  </Grid>
+                  <Divider />
+                  <Grid item>
+                    $3.0 / mo
+                  </Grid>
+                  <Grid item>
+                    $36.0
+                  </Grid>
+                  <Grid item>
+                    5 Devices
+                  </Grid>
+                  <Button variant="outlined" fullWidth={true}>
+                    Start Now
+                  </Button>
+                </Grid>
+              </Paper>
+            </Grid>
+          </Grid>
+          <Grid container spacing={24} xs={12} justify="center" style={{marginTop: 20}}>
+            <Grid className={classes.description} item>
+              <img src={logoWireguard} width="200" /><br />
+              Our service base on Wireguard aims to be faster, simpler, leaner, and more useful than IPSec.
             </Grid>
           </Grid>
         </div>
