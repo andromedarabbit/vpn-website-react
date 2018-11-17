ef2bb563a1f35d4c68be9d73bf6183ea63e8c4a9
Jason Park <binarydiver@gmail.com>
2018-11-17 21:50:27 +0900

    Polish price tag in the sign-up page.

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -4,7 +4,7 @@ import { withStyles } from '@material-ui/core/styles';
 import {
   Card,
   CardContent,
-  CardActions,
+  CardHeader,
   Button,
   Grid,
   Radio,
@@ -13,13 +13,38 @@ import {
 } from '@material-ui/core';
 import TopBar from '../components/TopBar';
 
-const styles = (theme) => ({
-  root: {
-    flexGrow: 1,
+const tiers = [
+  {
+    title: '1 Month',
+    price_monthly: '$5.0 / mo',
+    price_total: '$5.0',
+    device: '5 Devices',
   },
-  menuButton: {
-    marginLeft: -12,
-    marginRight: 20,
+  {
+    title: '3 Month',
+    price_monthly: '$4.0 / mo',
+    price_total: '$12.0',
+    device: '5 Devices',
+  },
+  {
+    title: '1 Year',
+    price_monthly: '$3.0 / mo',
+    price_total: '$36.0',
+    device: '5 Devices',
+  },
+];
+
+const styles = (theme) => ({
+  layout: {
+    width: 'auto',
+    marginLeft: theme.spacing.unit * 3,
+    marginRight: theme.spacing.unit * 3,
+    [theme.breakpoints.up(900 + theme.spacing.unit * 3 * 2)]: {
+      width: 900,
+      marginLeft: 'auto',
+      marginRight: 'auto',
+    },
+    paddingTop: 60,
   },
   containerGrid: {
     marginTop: window.matchMedia( "(min-width: 600px)" ) ? theme.mixins.toolbar["@media (min-width:600px)"].minHeight : theme.mixins.toolbar["@media (min-width:0px) and (orientation: landscape)"].minHeight
@@ -33,6 +58,7 @@ const styles = (theme) => ({
     display: 'flex',
     flexWrap: 'nowrap',
     flexDirection: 'column',
+    alignItems: 'center',
   },
   button: {
     margin: theme.spacing.unit,
@@ -49,7 +75,7 @@ class SignUp extends React.Component {
     this.state = {
       email: '',
       password: '',
-      selectedValue: 'a',
+      selectedValue: '1 Month',
     }
   }
 
@@ -67,133 +93,80 @@ class SignUp extends React.Component {
     const { classes } = this.props;
 
     return (
-      <div className={classes.root}>
+      <React.Fragment>
         <TopBar />
-        <Grid className={classes.containerGrid} justify="center" container style={{padding: 50}}>
-          <Grid item xs={12} style={{padding: 100}}>
-            <Grid container justify="center" spacing={32}>
-              <Grid item>
-                <Card>
-                  <Radio
-                    style={{position: 'absolute'}}
-                    checked={this.state.selectedValue === 'a'}
-                    onChange={this._handleChangeSubscription}
-                    value="a"
-                    name="radio-button-demo"
-                    aria-label="A"
-                  />
-                  <CardContent style={{textAlign: 'center', padding: 40}}>
-                    <Typography color="textSecondary" gutterBottom>
-                      Word of the Day
-                    </Typography>
-                    <Typography variant="h5" component="h2">
-                      A
-                    </Typography>
-                    <Typography color="textSecondary">
-                      B
-                    </Typography>
-                    <Typography component="p">
-                      C
-                    </Typography>
-                  </CardContent>
-                  <CardActions>
-                    <Button size="small">Learn More</Button>
-                  </CardActions>
-                </Card>
-              </Grid>
-              <Grid item>
-                <Card>
-                  <Radio
-                    style={{position: 'absolute'}}
-                    checked={this.state.selectedValue === 'b'}
-                    onChange={this._handleChangeSubscription}
-                    value="b"
-                    name="radio-button-demo"
-                    aria-label="B"
-                  />
-                  <CardContent style={{textAlign: 'center', padding: 40}}>
-                    <Typography color="textSecondary" gutterBottom>
-                      Word of the Day
-                    </Typography>
-                    <Typography variant="h5" component="h2">
-                      A
-                    </Typography>
-                    <Typography color="textSecondary">
-                      B
-                    </Typography>
-                    <Typography component="p">
-                      C
-                    </Typography>
-                  </CardContent>
-                  <CardActions>
-                    <Button size="small">Learn More</Button>
-                  </CardActions>
-                </Card>
-              </Grid>
-              <Grid item>
-                <Card>
-                  <Radio
-                    style={{position: 'absolute'}}
-                    checked={this.state.selectedValue === 'c'}
-                    onChange={this._handleChangeSubscription}
-                    value="c"
-                    name="radio-button-demo"
-                    aria-label="C"
-                  />
-                  <CardContent style={{textAlign: 'center', padding: 40}}>
-                    <Typography color="textSecondary" gutterBottom>
-                      Word of the Day
-                    </Typography>
-                    <Typography variant="h5" component="h2">
-                      A
-                    </Typography>
-                    <Typography color="textSecondary">
-                      B
-                    </Typography>
-                    <Typography component="p">
-                      C
-                    </Typography>
-                  </CardContent>
-                  <CardActions>
-                    <Button size="small">Learn More</Button>
-                  </CardActions>
-                </Card>
+        <main className={classes.layout}>
+          <Grid justify="center" container>
+            <Grid item xs={12}>
+              <Grid container justify="center" spacing={32}>
+                {
+                  tiers.map((tier, index) => (
+                    <Grid item key={index}>
+                      <Card>
+                        <Radio
+                          style={{ position: 'absolute' }}
+                          checked={this.state.selectedValue === tier.title}
+                          onChange={this._handleChangeSubscription}
+                          value={tier.title}
+                          name="radio-button-price"
+                          aria-label={tier.title}
+                        />
+                        <CardHeader
+                          style={{ paddingTop: 40 }}
+                          title={tier.title}
+                          titleTypographyProps={{ align: 'center' }}
+                        />
+                        <CardContent style={{textAlign: 'center', padding: '10px 60px'}}>
+                          <Typography color="textSecondary" gutterBottom>
+                            {tier.price_monthly}
+                          </Typography>
+                          <Typography variant="h6">
+                            {tier.price_total}
+                          </Typography>
+                          <Typography color="textSecondary">
+                            {tier.device}
+                          </Typography>
+                        </CardContent>
+                      </Card>
+                    </Grid>
+                  ))
+                }
               </Grid>
             </Grid>
+            <Grid item xs={12} style={{ padding: 50 }}>
+              <form className={classes.containerForm} noValidate autoComplete="off">
+                <TextField
+                  id="signup-email"
+                  label="E-mail"
+                  className={classes.textField}
+                  value={this.state.email}
+                  onChange={this.handleChange('email')}
+                  margin="normal"
+                />
+                <TextField
+                  id="signup-password"
+                  label="Password"
+                  className={classes.textField}
+                  type="password"
+                  autoComplete="current-password"
+                  margin="normal"
+                />
+                <TextField
+                  id="signup-confirm-password"
+                  label="Confirm Password"
+                  className={classes.textField}
+                  type="password"
+                  autoComplete="current-password"
+                  margin="normal"
+                />
+                <Button variant="contained" className={classes.button} style={{marginTop: 20}}>
+                  Register Account
+                </Button>
+              </form>
+            </Grid>
           </Grid>
-          <Grid item xs={12} style={{borderRight: "1px solid black", padding: 100}}>
-            <form className={classes.containerForm} noValidate autoComplete="off">
-              <TextField
-                id="signup-email"
-                label="E-mail"
-                className={classes.textField}
-                value={this.state.email}
-                onChange={this.handleChange('email')}
-                margin="normal"
-              />
-              <TextField
-                id="signup-password"
-                label="Password"
-                className={classes.textField}
-                type="password"
-                autoComplete="current-password"
-                margin="normal"
-              />
-              <TextField
-                id="signup-confirm-password"
-                label="Confirm Password"
-                className={classes.textField}
-                type="password"
-                autoComplete="current-password"
-                margin="normal"
-              />
-              <Button variant="contained" className={classes.button} style={{marginTop: 20}}>
-                Register Account
-              </Button>
-            </form>
-          </Grid>
-        </Grid>
-      </div>
+        </main>
+      </React.Fragment>
     );
   }
 }
